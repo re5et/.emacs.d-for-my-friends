1c35106c454de118b378090b4dbfa926f6aa261e
re5et <re5etsmyth@gmail.com>
2014-12-10 14:08:35 -0800

    Better backup defaults

--- my/my-backup.el
+++ my/my-backup.el
@@ -1,6 +1,10 @@
-(setq backup-directory-alist
-      '((".*" . "~/.emacs.d/.backups")))
-(setq auto-save-file-name-transforms
-      '((".*" "~/.emacs.d/.backups" t)))
-(setq tramp-backup-directory-alist backup-directory-alist)
+(setq
+ backup-by-copying t      ; don't clobber symlinks
+ backup-directory-alist '((".*" . "~/.emacs.d/.backups"))    ; don't litter my fs tree
+ auto-save-file-name-transforms '((".*" "~/.emacs.d/.backups" t))
+ tramp-backup-directory-alist backup-directory-alist
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)       ; use versioned backups
 (provide 'my-backup)
